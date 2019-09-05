5728e63158aa35e3912af776e4db95f9535fbff4
Daniel Dantas Marotti <dantasmaarotti@gmail.com>
2019-09-05 17:25:04 -0300

    Adding some files and managing structure

---
+++ src/main/java/com/beerhouse/controller/BeerController.java
@@ -0,0 +1,15 @@
+package com.beerhouse.controller;
+
+import com.beerhouse.model.Beer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class BeerController {
+
+  
+
+
+}
---
+++ src/main/java/com/beerhouse/model/Beer.java
@@ -0,0 +1,59 @@
+package com.beerhouse.model;
+
+public class Beer {
+
+    private int id;
+    private String name;
+    private String ingredients;
+    private String alcoholContent;
+    private float price;
+    private String category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getAlcoholContent() {
+        return alcoholContent;
+    }
+
+    public void setAlcoholContent(String alcoholContent) {
+        this.alcoholContent = alcoholContent;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
